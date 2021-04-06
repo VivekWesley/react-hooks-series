146fcf2551c84a13d579969e194c8d90df06cba9
VivekWesley <vivekjohnwesley@gmail.com>
2021-04-06 21:30:37 +0530

    ADD practice file 2

--- useEffect and numbersAPI/useFetch.js
+++ useEffect and numbersAPI/useFetch.js
@@ -14,21 +14,79 @@
 //     return state;
 // }
 
+// import React, { useState, useEffect } from 'react';
+
+// export const useFetch = (url) => {
+//     const [state, setState] = useState({
+//         data: null,
+//         loading: true
+//     });
+
+//     useEffect(() => {
+//         setState(state => ({ data: state.data, loading: true }));
+//         fetch(url).then(x => x.text())
+//             .then(y => {
+//                 setState({ data: y, loading: false });
+//             })
+
+//     }, [url]);
+//     return state;
+// }
+
+// import React, { useState, useEffect } from 'react';
+
+// export const useFetch = (url) => {
+//     const [state, setState] = useState({
+//         data: null,
+//         loading: true
+//     })
+
+//     useEffect(() => {
+//         setState(state => ({ data: state.data, loading: true }));
+//         fetch(url)
+//             .then(x => x.text())
+//             .then(y => {
+//                 setState({ data: y, loading: false });
+//             })
+//     }, [url])
+//     return state;
+// }
+
+// import React, { useState, useEffect } from 'react';
+
+// export const useFetch = (url) => {
+
+//     const [state, setState] = useState({
+//         data: null,
+//         loading: true
+//     })
+
+//     useEffect(() => {
+//         setState(state => ({ data: state.data, loading: true }));
+//         fetch(url)
+//             .then(x => x.text())
+//             .then(y => {
+//                 setState({ data: y, loading: false });
+//             })
+//     }, [url])
+//     return state;
+// }
+
 import React, { useState, useEffect } from 'react';
 
 export const useFetch = (url) => {
+
     const [state, setState] = useState({
         data: null,
         loading: true
-    });
+    })
 
     useEffect(() => {
-        setState(state => ({ data: state.data, loading: true }));
-        fetch(url).then(x => x.text())
+        fetch(url)
+            .then(x => x.text())
             .then(y => {
                 setState({ data: y, loading: false });
             })
-
-    }, [url]);
+    }, [url])
     return state;
 }
