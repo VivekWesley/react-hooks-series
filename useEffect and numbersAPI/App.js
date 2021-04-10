0b9c6ae6e22e95eafbc78673211c95f8e01822f7
VivekWesley <vivekjohnwesley@gmail.com>
2021-04-10 23:52:41 +0530

    ADD fetch data from randomuserapi/

--- useEffect and numbersAPI/App.js
+++ useEffect and numbersAPI/App.js
@@ -17,63 +17,74 @@ import Card from '@material-ui/core/Card';
 
 const App = () => {
 
-    const [values, handleChange] = useForm({
-        email: '',
-        password: '',
-    })
+  const [values, handleChange] = useForm({
+    email: '',
+    password: '',
+  })
 
-    const [count, setCount] = useState(0)
-    const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+  const [count, setCount] = useState(0)
+  const [person, setPerson] = useState(null);
 
-    // 1. these useEffect() hooks renders everytime page is loaded in order,  
-    useEffect(() => {
-        console.log('hello world 1')
-    }, [])
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
 
-    useEffect(() => {
-        console.log('hello world 2')
-    }, [])
-    // ______________________
+  // 1. these useEffect() hooks renders everytime page is loaded in order,  
+  useEffect(async () => {
+    const response = await fetch('https://api.randomuser.me/');
+    const data = await response.json();
+    const [item] = data.results;
+    setPerson(item);
+  }, [])
+
+  useEffect(() => {
+    console.log('hello world 2')
+  }, [])
+  // ______________________
 
 
-    // 2. this useEffect() renders everytime password is state changes
-    useEffect(() => {
-        console.log("password state changed")
-    }, [values.password])
-    // ______________________
+  // 2. this useEffect() renders everytime password is state changes
+  useEffect(() => {
+    console.log("password state changed")
+  }, [values.password])
+  // ______________________
 
-    return (
-        <div>
-            <div>
-                <Avatar>
-                    <ImageIcon />
-                </Avatar>
-            </div>
-            <div>
-                <TextField type="date" color="primary" variant="outlined" size="small" name="date" value={values.date} onChange={handleChange} />
-            </div>
-            <div>
-                <TextField type="time" color="primary" variant="outlined" size="small" name="time" value={values.time} onChange={handleChange} />
-            </div>
-            <div>
-                <TextField type="text" color="secondary" variant="outlined" size="small" name="email" label="Email" value={values.email} onChange={handleChange} />
-            </div>
-            <div>
-                <TextField type="password" variant="outlined" size="small" name="password" label="Password" value={values.password} onChange={handleChange} />
-            </div>
-            <div>
-                <Card variant="elevation">
-                    NumbersAPI DATA <br />
-                    <div>{count}</div>
-                    <div>{loading ? "loading..." : data}</div>
-                </Card>
-                <ButtonGroup size="small">
-                    <BUTTON startIcon={<SaveIcon />} variant="contained" color="primary" onClick={() => setCount(count => count + 1)}>increment</BUTTON>
-                    <BUTTON startIcon={<CancelIcon />} variant="contained" color="secondary" onClick={() => setCount(count => count - 1)}>decrement</BUTTON>
-                </ButtonGroup>
-            </div >
-        </div >
-    )
+  return (
+    <div>
+      <div>
+        <Avatar>
+          <ImageIcon />
+        </Avatar>
+      </div>
+      <div>
+        <TextField type="date" color="primary" variant="outlined" size="small" name="date" value={values.date} onChange={handleChange} />
+      </div>
+      <div>
+        <TextField type="time" color="primary" variant="outlined" size="small" name="time" value={values.time} onChange={handleChange} />
+      </div>
+      <div>
+        <TextField type="text" color="secondary" variant="outlined" size="small" name="email" label="Email" value={values.email} onChange={handleChange} />
+      </div>
+      <div>
+        <TextField type="password" variant="outlined" size="small" name="password" label="Password" value={values.password} onChange={handleChange} />
+      </div>
+      <div>
+        <Card variant="elevation">
+          NumbersAPI DATA <br />
+          <div>{count}</div>
+          <div>{loading ? "loading..." : data}</div>
+        </Card>
+        <ButtonGroup size="small">
+          <BUTTON startIcon={<SaveIcon />} variant="contained" color="primary" onClick={() => setCount(count => count + 1)}>increment</BUTTON>
+          <BUTTON startIcon={<CancelIcon />} variant="contained" color="secondary" onClick={() => setCount(count => count - 1)}>decrement</BUTTON>
+        </ButtonGroup>
+      </div>
+      {person && <div> Name: {person.name.first} &nbsp; &nbsp; {person.name.last}</div>}
+      {person && <div> Gender: {person.gender}</div>}
+      {person && <div> Email: {person.email}</div>}
+      {person && <div> City: {person.location.city}</div>}
+      {person && <div> State: {person.location.state}</div>}
+      {person && <div> Country: {person.location.coun}</div>}
+    </div>
+  )
 }
 
 export default App;
